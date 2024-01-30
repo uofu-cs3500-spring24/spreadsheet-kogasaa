35c5fcf94ebb045782deda9c15daf3662136004f
u1432163 <u1432163@eae-b760-129>
2024-01-29 20:44:58 -0700

    write the test to the Get variabels method

--- FormulaTests/FormulaTests.cs
+++ FormulaTests/FormulaTests.cs
@@ -96,7 +96,37 @@ namespace FormulaTests
                 Formula wrongVariable1 = new Formula("ewrw");
                 throw new ArgumentException("Formula Constructor Cannot Find Wrong Format");
             }
+            catch (FormulaFormatException)
+            {
+            }
 
+            try
+            {
+                Formula wrongVariable1 = new Formula("ewr3423ewrw");
+                throw new ArgumentException("Formula Constructor Cannot Find Wrong Format");
+            }
+            catch (FormulaFormatException)
+            {
+            }
+
+            try
+            {
+                Formula wrongVariable1 = new Formula("3453ewrw");
+                throw new ArgumentException("Formula Constructor Cannot Find Wrong Format");
+            }
+            catch (FormulaFormatException)
+            {
+            }
+
+            try
+            {
+                Formula wrongVariable1 = new Formula("440de9ufwjiojd");
+                throw new ArgumentException("Formula Constructor Cannot Find Wrong Format");
+            }
+            catch (FormulaFormatException)
+            {
+            }
+        }
         /// <summary>
         /// These are all right format and should never throw a Exception
         /// </summary>
@@ -108,7 +138,10 @@ namespace FormulaTests
             Formula rightFormat2 = new Formula("a1+i2-d1*(2)/2+9-2-2/(((((w3)))))*2");
             Formula rightFormat3 = new Formula("1+22/3*4-2+y2");
             Formula rightFormat4 = new Formula("2");
-            Formula rightFormat5 = new Formula("variable,and_this_is_super_long");
+            Formula rightFormat5 = new Formula("_a_1");
+            Formula rightFormat6 = new Formula("qwe_123");
+            Formula rightFormat7 = new Formula("qwe_123____33awe_8");
+            Formula rightFormat8 = new Formula("____3");
         }
 
         /// <summary>
@@ -119,6 +152,62 @@ namespace FormulaTests
         public void TestWhenNormalizeToWrong()
         {
             Formula wrongFormat = new Formula("9+y3", n => "3Y", v => true);
+        }
+
+        /// <summary>
+        /// Throw a exception when normalized variables are illegal!
+        /// </summary>
+        [TestMethod, Timeout(5000)]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void TestWhenValidorIsWrong()
+        {
+            Formula wrongFormat = new Formula("9+y3", n => n, v => false);
+        }
+
+        /// <summary>
+        /// The Normalized Result can be different with not normliazed result.
+        /// </summary>
+        [TestMethod, Timeout(5000)]
+        public void TestIfLookUpNormalized()
+        {
+            Formula formula1 = new Formula("9+y3+Y3", n => n.ToUpper(), v => true);
+            double result1 = (double)formula1.Evaluate(a => { if (a == "y3") { return 1000; } else { return 10000; } } );
+            Assert.AreEqual(20009, result1);
+
+            Formula formula2 = new Formula("9+y3+Y3");
+            double result2 = (double)formula1.Evaluate(a => { if (a == "y3") { return 1000; } else { return 10000; } });
+            Assert.AreEqual(11009, result2 );
+        }
+
+        /// <summary>
+        /// This will test edge cases;
+        ///     1. when lookup throws a exception
+        ///     2. when there is divide by zero
+        /// Those cases will all return a Formula Error
+        /// </summary>
+        [TestMethod, Timeout(5000)]
+        public void TestEvaluateReturnError()
+        {
+            Formula divdeBy0 = new Formula("8/(1-1)");
+            Formula normal = new Formula("d1");
+            Assert.AreEqual(divdeBy0.Evaluate(s=>1).GetType(), typeof(FormulaError));
+            Assert.AreEqual(normal.Evaluate(s=>{ throw new ArgumentException(); }).GetType(), typeof(FormulaError));
+        }
+
+        /// <summary>
+        /// This will test Get Variables
+        /// and the variables are must be normalized
+        /// </summary>
+        [TestMethod, Timeout(5000)]
+        public void TestGetVariables()
+        {
+            Formula formula1 = new Formula("a1+A1+c3+d4");
+            Formula formula2 = new Formula("a1+A1+c3+d4", n=>n.ToUpper(), v => true);
+            HashSet<string> variables1 = new HashSet<string> {"a1", "A1", "c3", "d4"};
+            HashSet<string> variables2 = new HashSet<string> {"A1", "C3", "D4"};
+            
+            Assert.AreEqual(formula1.GetVariables(), variables1);
+            Assert.AreEqual(formula2.GetVariables(), variables2);
         }
 
     }
